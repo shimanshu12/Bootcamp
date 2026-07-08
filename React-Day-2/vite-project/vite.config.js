1aa01132f6f544ee62ebac98b9c0d8337d1473cb
shimanshu12 <143255221+shimanshu12@users.noreply.github.com>
2026-07-08 14:25:24 +0530

    Fix Vercel deployment base path

--- React-Day-2/vite-project/vite.config.js
+++ React-Day-2/vite-project/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT: repo name = Bootcamp
 export default defineConfig({
   plugins: [react()],
-  base: '/Bootcamp/',
+  base: '/',
 })
